2fb69fd218b20bc9477a81041ec595b077374b98
Espinozaje <jeisson12aaron@gmail.com>
2025-05-08 04:10:06 -0500

    Crear cliente de aplicación para el frontend

--- Iac/waf-api.tf
+++ Iac/waf-api.tf
@@ -21,3 +21,24 @@ resource "aws_cognito_user_group" "user_group" {
   description  = "Usuarios normales"
   precedence   = 2
 }
+
+resource "aws_cognito_user_pool_client" "frontend_client" {
+  name         = "frontend-client"
+  user_pool_id = aws_cognito_user_pool.main_pool.id
+  generate_secret = false
+
+  allowed_oauth_flows_user_pool_client = true
+
+  allowed_oauth_flows = ["code"] # Puedes usar "implicit" también si no usas backend
+
+  allowed_oauth_scopes = [
+    "openid",
+    "email",
+    "profile"
+  ]
+
+  supported_identity_providers = ["COGNITO"]
+
+  callback_urls = ["https://dhdqrwclpir4.cloudfront.net"]
+  logout_urls   = ["https://dhdqrwclpir4.cloudfront.net"]
+}
