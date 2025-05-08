7e7c2700a47bdfb13014638d01534167235779fc
Lourdes Angulo Renteria <langulor2@upao.edu.pe>
2025-05-08 03:37:53 -0500

    Primer Recurso

--- Iac/lambda-taxis.tf
+++ Iac/lambda-taxis.tf
@@ -3,3 +3,13 @@ data "archive_file" "lambda_taxis" {
   source_dir  = "${path.module}/../taxis"
   output_path = "${path.module}/bin/taxis.zip"
 }
+
+resource "aws_lambda_function" "taxis" {
+  function_name    = "taxis"
+  handler          = "index.handler"
+  runtime          = "nodejs16.x"
+  role             = aws_iam_role.lambda_taxis_exec_role.arn // El arn es el ID para conectar el rol con el recurso
+  filename         = data.archive_file.lambda_taxis.output_path
+  source_code_hash = data.archive_file.lambda_taxis.output_base64sha512
+
+}
