580b638c4e0f918b1d33ac243c5e38186416fa18
YulianaCacho <ycachoq1@upao.edu.pe>
2025-05-08 09:58:15 -0500

    Declaración de rol

--- Iac/lambda-ususarios.tf
+++ Iac/lambda-ususarios.tf
@@ -4,6 +4,22 @@ data "archive_file" "lambda_usuarios" {
   output_path = "${path.module}/bin/usuarios.zip"
 }
 
+resource "aws_iam_role" "lambda_usuarios_exec_role" { //Rol Necesario para ejecutar el recurso lambda
+  name = "usuarios_exec_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 resource "aws_lambda_function" "usuarios" {
   function_name    = "usuarios"
   handler          = "index.handler"
