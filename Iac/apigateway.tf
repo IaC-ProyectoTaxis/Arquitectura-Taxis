4499be760ff45fcd2b59a3dc93511165d6cc3efc
PieroSanchez <pierosancas@gmail.com>
2025-05-08 14:22:05 -0500

    integrar lambda al POST y creacion de deployment con stage prod

--- Iac/apigateway.tf
+++ Iac/apigateway.tf
@@ -15,3 +15,31 @@ resource "aws_api_gateway_method" "post_registro" {
   http_method   = "POST"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "lambda_post_registro_usuario" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.registro.id
+  http_method = aws_api_gateway_method.post_registro.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.usuarios.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gw_invoke_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.usuarios.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}//"
+}
+
+resource "aws_api_gateway_deployment" "api_deploy" {
+  depends_on  = [aws_api_gateway_integration.lambda_post_registro_usuario]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+
+  triggers = {
+    redeploy = timestamp()
+  }
+}
