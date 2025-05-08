f4517591da47cb61aea0520bb9a9c19931fdfe92
YulianaCacho <ycachoq1@upao.edu.pe>
2025-05-08 09:57:07 -0500

    Primer recurso

--- Iac/lambda-ususarios.tf
+++ Iac/lambda-ususarios.tf
@@ -3,3 +3,13 @@ data "archive_file" "lambda_usuarios" {
   source_dir  = "${path.module}/../usuarios"
   output_path = "${path.module}/bin/usuarios.zip"
 }
+
+resource "aws_lambda_function" "usuarios" {
+  function_name    = "usuarios"
+  handler          = "index.handler"
+  runtime          = "nodejs16.x"
+  role             = aws_iam_role.lambda_usuarios_exec_role.arn // El arn es el ID para conectar el rol con el recurso
+  filename         = data.archive_file.lambda_usuarios.output_path
+  source_code_hash = data.archive_file.lambda_usuarios.output_base64sha512
+  
+}
