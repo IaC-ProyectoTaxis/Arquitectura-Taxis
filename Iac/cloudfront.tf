fa8cb04f1c1cd8992067e586385ed893adbbe176
Espinozaje <jeisson12aaron@gmail.com>
2025-05-08 03:54:37 -0500

    Crear control de acceso a origen (OAC) para S3

---
+++ Iac/cloudfront.tf
@@ -0,0 +1,7 @@
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "cloudfront-s3-oac"
+  description                       = "Access control for S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
