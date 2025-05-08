a7446bb7b24201dc7b611fda4d01c9d24f136e98
Espinozaje <jeisson12aaron@gmail.com>
2025-05-08 14:38:10 -0500

    Implementación WAF final

--- Iac/waf-api.tf
+++ Iac/waf-api.tf
@@ -1,44 +1,41 @@
-resource "aws_cognito_user_pool" "main_pool" {
-  name = "usuarios-pool"
-
-  admin_create_user_config {
-    allow_admin_create_user_only = true
+resource "aws_wafv2_web_acl" "api_waf_acl" {
+  name        = "api-waf-acl"
+  description = "WAF para proteger el API Gateway"
+  scope       = "REGIONAL" # Para API Gateway o ALB, siempre "REGIONAL"
+  default_action {
+    allow {}
   }
 
-  auto_verified_attributes = ["email"]
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "apiWaf"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "limit-requests"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "limitRequests"
+      sampled_requests_enabled   = true
+    }
+  }
 }
 
-resource "aws_cognito_user_group" "admin_group" {
-  name         = "admin"
-  user_pool_id = aws_cognito_user_pool.main_pool.id
-  description  = "Administradores del sistema"
-  precedence   = 1
-}
-
-resource "aws_cognito_user_group" "user_group" {
-  name         = "user"
-  user_pool_id = aws_cognito_user_pool.main_pool.id
-  description  = "Usuarios normales"
-  precedence   = 2
-}
-
-resource "aws_cognito_user_pool_client" "frontend_client" {
-  name         = "frontend-client"
-  user_pool_id = aws_cognito_user_pool.main_pool.id
-  generate_secret = false
-
-  allowed_oauth_flows_user_pool_client = true
-
-  allowed_oauth_flows = ["code"] # Puedes usar "implicit" también si no usas backend
-
-  allowed_oauth_scopes = [
-    "openid",
-    "email",
-    "profile"
-  ]
-
-  supported_identity_providers = ["COGNITO"]
-
-  callback_urls = ["https://dhdqrwclpir4.cloudfront.net"]
-  logout_urls   = ["https://dhdqrwclpir4.cloudfront.net"]
+resource "aws_wafv2_web_acl_association" "api_waf_assoc" {
+  resource_arn = "arn:aws:apigateway:us-east-2::/restapis/${aws_api_gateway_rest_api.api.id}/stages/${aws_api_gateway_deployment.api_deploy.stage_name}"
+  web_acl_arn  = aws_wafv2_web_acl.api_waf_acl.arn
 }
