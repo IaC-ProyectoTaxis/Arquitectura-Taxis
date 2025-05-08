86f559bc212d3c47451c34c299fb895dc24b6f10
Yamir Vargas Alvitres <yvargasa2@upao.edu.pe>
2025-05-08 03:16:44 -0500

    Primer recurso

--- Iac/lambda-viajes.tf
+++ Iac/lambda-viajes.tf
@@ -3,3 +3,12 @@ data "archive_file" "lambda_viajes" {
   source_dir  = "${path.module}/../viajes"
   output_path = "${path.module}/bin/viajes.zip"
 }
+resource "aws_lambda_function" "viajes" {
+  function_name    = "viajes"
+  handler          = "index.handler"
+  runtime          = "nodejs16.x"
+  role             = aws_iam_role.lambda_viajes_exec_role.arn // El arn es el ID para conectar el rol con el recurso
+  filename         = data.archive_file.lambda_viajes.output_path
+  source_code_hash = data.archive_file.lambda_viajes.output_base64sha512
+
+}
