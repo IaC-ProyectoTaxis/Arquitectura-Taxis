974d20cf900ea852492b47e1a9e8df1a57fc030e
PieroSanchez <pierosancas@gmail.com>
2025-05-08 14:20:14 -0500

    definir metodo POST en registro (sin autorizacion)

--- Iac/apigateway.tf
+++ Iac/apigateway.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_resource" "registro" {
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "registro"
 }
+
+resource "aws_api_gateway_method" "post_registro" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.registro.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
