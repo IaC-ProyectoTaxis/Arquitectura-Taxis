4a1b3a2d2a55da34ef869460ad7464876659ddec
PieroSanchez <pierosancas@gmail.com>
2025-06-13 23:14:07 -0500

    actualizar-version-nodejs

--- Iac/lambda-taxis.tf
+++ Iac/lambda-taxis.tf
@@ -51,7 +51,7 @@ resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" { //
 resource "aws_lambda_function" "taxis" {
   function_name    = "taxis"
   handler          = "index.handler"
-  runtime          = "nodejs16.x"
+  runtime          = "nodejs18.x"
   role             = aws_iam_role.lambda_taxis_exec_role.arn // El arn es el ID para conectar el rol con el recurso
   filename         = data.archive_file.lambda_taxis.output_path
   source_code_hash = data.archive_file.lambda_taxis.output_base64sha512
