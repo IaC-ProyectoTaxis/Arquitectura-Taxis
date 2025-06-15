c943b11ffe9a294d59a6ca824c6d374db99eda21
espinozaje <jeisson12aaron@gmail.com>
2025-06-14 21:07:17 -0500

    bloqueo de acceso público

--- Iac/S3.tf
+++ Iac/S3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "taxis-bucket"
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  ignore_public_acls  = true
+  block_public_policy = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.bucket.id
