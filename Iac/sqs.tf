9260ddf4af2dee661dd1a30565f5e2b6515badc1
PieroSanchez <pierosancas@gmail.com>
2025-06-19 13:59:00 -0500

    Añadir políticas de colas SQS para usuarios, taxis y viajes con permisos para enviar mensajes desde el tópico SNS de eventos de la aplicación

--- Iac/sqs.tf
+++ Iac/sqs.tf
@@ -9,3 +9,63 @@ resource "aws_sqs_queue" "taxis_queue" {
 resource "aws_sqs_queue" "viajes_queue" {
   name = "viajes-queue"
 }
+
+resource "aws_sqs_queue_policy" "usuarios_queue_policy" {
+  queue_url = aws_sqs_queue.usuarios_queue.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "",
+        Action = "sqs:SendMessage",
+        Resource = aws_sqs_queue.usuarios_queue.arn,
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.app_events.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "taxis_queue_policy" {
+  queue_url = aws_sqs_queue.taxis_queue.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "",
+        Action = "sqs:SendMessage",
+        Resource = aws_sqs_queue.taxis_queue.arn,
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.app_events.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "viajes_queue_policy" {
+  queue_url = aws_sqs_queue.viajes_queue.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "sqs:SendMessage",
+        Resource = aws_sqs_queue.viajes_queue.arn,
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.app_events.arn
+          }
+        }
+      }
+    ]
+  })
+}
