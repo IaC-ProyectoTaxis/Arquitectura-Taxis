347e01c5213efca9fd8cc1cf4887fdcaa36ff26e
PieroSanchez <pierosancas@gmail.com>
2025-06-19 13:56:00 -0500

    Añadir suscripciones SQS para el tópico de eventos de la aplicación

--- Iac/sns.tf
+++ Iac/sns.tf
@@ -1,3 +1,21 @@
 resource "aws_sns_topic" "app_events" {
   name = "app-events-topic"
 }
+
+resource "aws_sns_topic_subscription" "usuarios_sub" {
+  topic_arn = aws_sns_topic.app_events.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.usuarios_queue.arn
+}
+
+resource "aws_sns_topic_subscription" "taxis_sub" {
+  topic_arn = aws_sns_topic.app_events.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.taxis_queue.arn
+}
+
+resource "aws_sns_topic_subscription" "viajes_sub" {
+  topic_arn = aws_sns_topic.app_events.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.viajes_queue.arn
+}
